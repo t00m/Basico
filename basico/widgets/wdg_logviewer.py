4fde4b85650adc2995beeb92e9602eed5f2764e0
Tomás Vírseda <tomasvirseda@gmail.com>
2020-06-07 12:41:53 +0200

    Logviewer working as expected

--- basico/widgets/wdg_logviewer.py
+++ basico/widgets/wdg_logviewer.py
@@ -15,6 +15,7 @@ from enum import IntEnum
 import gi
 gi.require_version('Gtk', '3.0')
 gi.require_version('Gdk', '3.0')
+from gi.repository import GLib
 from gi.repository import Gtk
 from gi.repository import Gdk
 from gi.repository import Pango
@@ -24,9 +25,12 @@ from basico.core.mod_wdg import BasicoWidget
 from basico.core.mod_env import ROOT, USER_DIR, APP, LPATH, GPATH, FILE
 
 class COLUMN(IntEnum):
-    KEY = 0
-    ICON = 1
-    CHECKBOX = 2
+    LEVELNO = 0
+    LEVELNAME = 1
+    MODULE = 2
+    TS_SECONDS = 3
+    TS_HUMAN = 4
+    MESSAGE = 5
 
 
 class LogViewer(BasicoWidget, Gtk.Box):
@@ -54,31 +58,42 @@ class LogViewer(BasicoWidget, Gtk.Box):
         treeview = self.srvgui.add_widget('logviewer_treeview', Gtk.TreeView())
         scr.add(treeview)
         visor.pack_start(scr, True, True, 0)
-        visor.show_all()
+        self.add(visor)
+
 
         # Setup model
         model = Gtk.ListStore(
-            str,        # key
-            str,        # Icon
-            str,        # checkbox
+            int,        # level number
+            str,        # level name
+            str,        # module
+            int,        # timestamp seconds
+            str,        # timestamp human
+            str,        # message
         )
         self.srvgui.add_widget('gtk_model_logviewer', model)
 
         # Setup columns
         def get_column_header_widget(title, icon_name=None, width=28, height=28):
             hbox = Gtk.HBox()
-            # ~ icon = self.srvicm.get_new_image_icon(icon_name, width, height)
             label = Gtk.Label()
             label.set_markup("<b>%s</b>" % title)
-            # ~ label.modify_font(Pango.FontDescription('Monospace 10'))
-            # ~ hbox.pack_start(icon, False, False, 3)
             hbox.pack_start(label, True, True, 3)
             hbox.show_all()
             return hbox
 
-        # SAP Note key
-        renderer = self.srvgui.add_widget('logviewer_renderer_key', Gtk.CellRendererText())
-        column = self.srvgui.add_widget('logviewer_column_key', Gtk.TreeViewColumn('Key', renderer, text=COLUMN.KEY))
+        # Level No.
+        renderer = self.srvgui.add_widget('logviewer_renderer_levelno', Gtk.CellRendererText())
+        column = self.srvgui.add_widget('logviewer_column_levelno', Gtk.TreeViewColumn('Level No.', renderer, text=COLUMN.LEVELNO))
+        renderer.set_property('height', 32)
+        column.set_visible(False)
+        column.set_expand(False)
+        column.set_clickable(False)
+        column.set_sort_indicator(False)
+        treeview.append_column(column)
+
+        # Level Name
+        renderer = self.srvgui.add_widget('logviewer_renderer_levelname', Gtk.CellRendererText())
+        column = self.srvgui.add_widget('logviewer_column_levelname', Gtk.TreeViewColumn('Level', renderer, text=COLUMN.LEVELNAME))
         renderer.set_property('height', 32)
         column.set_visible(True)
         column.set_expand(False)
@@ -86,9 +101,9 @@ class LogViewer(BasicoWidget, Gtk.Box):
         column.set_sort_indicator(False)
         treeview.append_column(column)
 
-        # SAP Note key
-        renderer = self.srvgui.add_widget('logviewer_renderer_icon', Gtk.CellRendererText())
-        column = self.srvgui.add_widget('logviewer_column_key', Gtk.TreeViewColumn('Icon', renderer, text=COLUMN.ICON))
+        # Module
+        renderer = self.srvgui.add_widget('logviewer_renderer_module', Gtk.CellRendererText())
+        column = self.srvgui.add_widget('logviewer_column_module', Gtk.TreeViewColumn('Module', renderer, text=COLUMN.MODULE))
         renderer.set_property('height', 32)
         column.set_visible(True)
         column.set_expand(False)
@@ -96,18 +111,61 @@ class LogViewer(BasicoWidget, Gtk.Box):
         column.set_sort_indicator(False)
         treeview.append_column(column)
 
-        # SAP Note key
-        renderer = self.srvgui.add_widget('logviewer_renderer_checkbox', Gtk.CellRendererText())
-        column = self.srvgui.add_widget('logviewer_column_checkbox', Gtk.TreeViewColumn('Checkbox', renderer, text=COLUMN.CHECKBOX))
+        # Timestamp seconds
+        renderer = self.srvgui.add_widget('logviewer_renderer_ts_seconds', Gtk.CellRendererText())
+        column = self.srvgui.add_widget('logviewer_column_ts_seconds', Gtk.TreeViewColumn('Timestamp', renderer, text=COLUMN.TS_SECONDS))
+        renderer.set_property('height', 32)
+        column.set_visible(False)
+        column.set_expand(False)
+        column.set_clickable(False)
+        column.set_sort_indicator(False)
+        treeview.append_column(column)
+
+        # Timestamp human
+        renderer = self.srvgui.add_widget('logviewer_renderer_ts_human', Gtk.CellRendererText())
+        column = self.srvgui.add_widget('logviewer_column_ts_human', Gtk.TreeViewColumn('Timestamp', renderer, text=COLUMN.TS_HUMAN))
+        renderer.set_property('height', 32)
+        column.set_visible(True)
+        column.set_expand(False)
+        column.set_clickable(False)
+        column.set_sort_indicator(False)
+        treeview.append_column(column)
+
+        # Message
+        renderer = self.srvgui.add_widget('logviewer_renderer_message', Gtk.CellRendererText())
+        column = self.srvgui.add_widget('logviewer_column_message', Gtk.TreeViewColumn('Message', renderer, markup=COLUMN.MESSAGE))
         renderer.set_property('height', 32)
         column.set_visible(True)
         column.set_expand(True)
         column.set_clickable(False)
         column.set_sort_indicator(False)
         treeview.append_column(column)
+        self.column_message = column
 
+        selection = treeview.get_selection()
+        self.srvgui.add_widget('logviewer_selection', selection)
+        selection.set_mode(Gtk.SelectionMode.SINGLE)
+
+        treeview.set_grid_lines(Gtk.TreeViewGridLines.BOTH)
+        treeview.set_enable_tree_lines(True)
+        treeview.set_model(model)
+        self.show_all()
 
     def update(self, *args):
+        treeview = self.srvgui.get_widget('logviewer_treeview')
+        model = self.srvgui.get_widget('gtk_model_logviewer')
+        selection = self.srvgui.get_widget('logviewer_selection')
         record = args[1]
-        print ("LOGVIEWER: %s" % record.getMessage())
 
+        def insert(record):
+            levelno = record.levelno
+            levelname = record.levelname
+            module = record.module
+            tss = record.created
+            tsh = record.asctime
+            message = record.getMessage()
+            riter = model.insert(0, (levelno, levelname, module, tss, tsh, message))
+            selection.select_iter(riter)
+            treeview.scroll_to_cell(0, self.column_message)
+
+        GLib.idle_add(insert, record)
