f9e474a520be887bbd846a6ded5cfbb6240e63c7
t00m <tomasvirseda@gmail.com>
2020-06-16 10:07:21 +0200

    Fix signals. Temporary commit

--- basico/widgets/visor_sapnotes.py
+++ basico/widgets/visor_sapnotes.py
@@ -729,16 +729,6 @@ class SAPNotesVisor(BasicoWidget, Gtk.VBox):
             # ~ button.connect('clicked', self.srvclb.gui_attachment_add_to_sapnote, sid)
             fbox.pack_start(button, False, False, 0)
 
-            # Popover button "Show attachments"
-            # ~ if len(self.srvatc.get_by_sid(sid)) > 0:
-                # ~ button = get_popover_button("<b>Show</b> linked to SAP Note %d" % isid, 'basico-attachment')
-                # ~ button.set_property('margin', 3)
-                # ~ button.show_all()
-                # ~ # button.connect('clicked', self.srvclb.gui_attachment_show_, sid)
-                # ~ fbox.pack_start(button, False, False, 0)
-            # ~ frame.add(fbox)
-            # ~ box.pack_start(frame, False, False, 0)
-
             # Popover button "Open SAP Note"
             button = get_popover_button("<b>Browse</b> SAP Note %d" % isid, 'basico-preview')
             button.connect('clicked', self.srvweb.browse_note, sid)
