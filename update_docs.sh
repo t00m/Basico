3db61e42332e4b01131ae0b8aa64ea77d92b1b8f
t00m <tomasvirseda@gmail.com>
2020-07-21 00:40:46 +0200

    Update docs

--- update_docs.sh
+++ update_docs.sh
@@ -1,1 +1,5 @@
-$HOME/.local/bin/kb4it -source docs/sources/ -target docs/html/ -log DEBUG -force
+# Update Github pages
+$HOME/.local/bin/kb4it -source basico/data/help/sources/ -target docs/ -log DEBUG -force
+
+# Update Basico Help Center
+$HOME/.local/bin/kb4it -source basico/data/help/sources/ -target basico/data/help/html/ -log DEBUG -force
