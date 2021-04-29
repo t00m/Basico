9eb0181c84eb7808da13bc84090cbaefc75bb069
t00m <tomasvirseda@gmail.com>
2021-04-29 10:19:09 +0200

    Add example plugin

---
+++ basico/data/plugins/plugin2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Interfaces
+
+import basico.core.plg as plugintypes
+
+class PluginSAP(plugintypes.IBasicoSAP):
+    def init(self, app):
+        self.app = app
+
+    def run(self, *args):
+        # ~ self.add_category_filter("Database", plugintypes.IBasicoDatabase)
+        self.log.debug("This is a plugin for SAP with args %s", args)
+
